a3ed4fba43cce8cf0de309bd90daedfcae5eb26b
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-01-21 11:06:33 +0100

    docs: add shape to example

--- examples/ohayo.rs
+++ examples/ohayo.rs
@@ -9,9 +9,12 @@ fn main() {
     // A tensor is generic over it's Rank, It's DType and It's Device
     let _tensor_decl: Tensor<2, f32, CpuDevice>;
 
+    // A tensor's shape is just an array ([`[usize; N]`])
+    let scalar_shape = [1];
+
     // Create an empty tensor by using [`Tensor::empty`]. The tensor will be allocated but unitialized.
-    let _empty_tensor = Tensor::<1, f32, CpuDevice>::empty([1]);
+    let _empty_tensor = Tensor::<1, f32, CpuDevice>::empty(scalar_shape);
 
     // Create a zero intialized tensor by using [`Tensor::zeros`].
-    let _zero_tensor = Tensor::<1, f32, CpuDevice>::zeros([1]);
+    let _zero_tensor = Tensor::<1, f32, CpuDevice>::zeros(scalar_shape);
 }
